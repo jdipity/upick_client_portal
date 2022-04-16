98e58a7f11d3917d656590af5c140f389676be36
jdipity <stevenson.jaron@gmail.com>
2022-04-16 16:18:54 -0400

    login

---
+++ src/components/UserLogin.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-card square bordered class="q-pa-lg shadow-1">
+    <q-card-section>
+      <q-form class="q-gutter-md">
+        <q-input
+          square
+          filled
+          clearable
+          v-model="email"
+          type="email"
+          label="email"
+        />
+        <q-input
+          square
+          filled
+          clearable
+          v-model="password"
+          type="password"
+          label="password"
+        />
+      </q-form>
+    </q-card-section>
+    <q-card-actions class="q-px-md">
+      <q-btn
+        unelevated
+        color="red"
+        size="lg"
+        class="full-width"
+        label="Login"
+      />
+    </q-card-actions>
+    <q-card-section class="text-center q-pa-none">
+      <p class="text-grey-6">Not reigistered? Created an Account</p>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "UserLogin",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
+
+<style>
+.q-card {
+  width: 360px;
+}
+</style>
